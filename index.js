6cb2f8519ce90caa96b1a449c3022eca341c869d
jiggag <jiggag90@gmail.com>
2022-07-01 23:47:21 +0900

    feat: index, package.json

---
+++ index.js
@@ -0,0 +1,44 @@
+import React, {memo, useMemo, useRef} from 'react';
+import {Animated, View} from 'react-native';
+
+export const WithHighlight = memo(({color = 'red', children}) => {
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  Animated.timing(opacity, {
+    toValue: 1,
+    duration: 150,
+    useNativeDriver: true,
+  }).start(() => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 150,
+      useNativeDriver: true,
+    }).start();
+  });
+
+  const animatedStyle = useMemo(
+    () => ({
+      opacity: opacity.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 1],
+      }),
+      borderColor: color,
+      borderWidth: 2,
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    }),
+    [opacity, color],
+  );
+
+  return useMemo(() => {
+    return (
+      <View>
+        <Animated.View style={animatedStyle} />
+        {children}
+      </View>
+    );
+  }, [animatedStyle, children]);
+});
